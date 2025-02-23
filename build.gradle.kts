632e677342ea8efb8014ac0d298635e04a5851a8
赵日天 <truenine304520@gmail.com>
2025-02-23 09:42:58 +0800

    build(gradle): 配置 Kotlin 项目构建
    - 添加 Kotlin、Spring Boot 和其他相关插件- 设置 Java 版本和 Kotlin 编译选项
    - 配置测试任务使用 JUnit Platform- 更新 settings.gradle.kts 以管理插件版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,45 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
   java
+  alias(libs.plugins.org.springframework.boot)
+  alias(libs.plugins.org.jetbrains.kotlin.jvm)
+  alias(libs.plugins.org.jetbrains.kotlin.kapt)
+  alias(libs.plugins.org.jetbrains.kotlin.plugin.spring)
+  alias(libs.plugins.com.google.devtools.ksp)
+  alias(libs.plugins.io.spring.dependency.management)
+}
+
+group = "org.example"
+version = "1.0"
+
+repositories {
+  mavenCentral()
+  maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+java {
+  sourceCompatibility = JavaVersion.toVersion(libs.versions.java.get())
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(libs.versions.java.get()))
+  }
+}
+
+kapt {
+  correctErrorTypes = true
+  keepJavacAnnotationProcessors = true
+}
+
+kotlin {
+  compilerOptions {
+    jvmTarget = JvmTarget.fromTarget(libs.versions.java.get())
+    freeCompilerArgs = listOf(
+      "-Xjsr305=strict", "-Xjvm-default=all", "-verbose", "-Xjdk-release=${libs.versions.java.get()}", "-jvm-target=${libs.versions.java.get()}"
+    )
+  }
+  jvmToolchain(libs.versions.java.get().toInt())
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
 }
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+pluginManagement {
+  repositories {
+    repositories {
+      gradlePluginPortal()
+      maven { url = uri("https://repo.spring.io/milestone") }
+    }
+  }
+}
