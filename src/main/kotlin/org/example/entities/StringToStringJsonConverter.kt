5e5ec70e09e24a2b6f15213518892695bcbfc2ca
赵日天 <truenine304520@gmail.com>
2025-03-09 15:04:31 +0800

    refactor(jimmer-playground):升级 Jimmer 版本并优化实体转换器
    - 将 Jimmer 版本从 0.9.57 升级到 0.9.62
    - 优化 ATable.dto 文件结构，增加输入 DTO 定义
    - 简化 StringToStringJsonConverter 中的空值处理逻辑
    - 更新 Kotlin 和 KSP版本

--- src/main/kotlin/org/example/entities/StringToStringJsonConverter.kt
+++ src/main/kotlin/org/example/entities/StringToStringJsonConverter.kt
@@ -3,11 +3,11 @@ package org.example.entities
 import org.babyfish.jimmer.jackson.Converter
 
 class StringToStringJsonConverter : Converter<String, String> {
-  override fun output(value: String?): String {
-    return value ?: ""
+  override fun output(value: String): String {
+    return value
   }
 
-  override fun input(jsonValue: String?): String {
-    return jsonValue ?: ""
+  override fun input(jsonValue: String): String {
+    return jsonValue
   }
 }
